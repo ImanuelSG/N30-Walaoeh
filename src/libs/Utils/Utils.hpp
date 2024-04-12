0e9c51ea5054bac721b32e0cc709a11cc4f43e35
Julian <julianchandrasutadi@gmail.com>
2024-04-12 11:49:00 +0700

    Revert "delete redundant function"
    This reverts commit f2d4e274e21276120ef403a14dd256a605a4073c.

--- src/libs/Utils/Utils.hpp
+++ src/libs/Utils/Utils.hpp
@@ -23,3 +23,4 @@ string toLowercase(const string &str);
 void displayReadyPanen(map<string, tuple<list<string>, int>> &readyItems);
 void countdown(int seconds);
 string intToAlphabet(int n);
+void displayReadyPanen(map<string, tuple<vector<string>, int>> &readyItems);
